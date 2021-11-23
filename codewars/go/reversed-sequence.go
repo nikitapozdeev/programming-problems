a783ee31245f1318b9b32f01c53d9c6737285316
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-11-23 22:34:31 +0400

    add codewars kata for go

---
+++ codewars/go/reversed-sequence.go
@@ -0,0 +1,17 @@
+/*
+  Level: 8 kyu
+  Link: https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+  Instructions:
+    Build a function that returns an array of integers from n to 1 where n>0.
+    Example : n=5 --> [5,4,3,2,1]
+*/
+
+package kata
+
+func ReverseSeq(n int) []int {
+  arr := make([]int, n)
+  for i:= 0; i < n; i++ {
+    arr[i] = n - i
+  }
+  return arr
+}
