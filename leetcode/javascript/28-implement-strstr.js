f885d0fdc5d17862de8d3a44937838e720786de5
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-07-04 21:44:36 +0400

    add implement strstr solution

---
+++ leetcode/javascript/28-implement-strstr.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/implement-strstr/
+ */
+
+// TODO: this is brutforce solution, read about KMP
+ 
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+  
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        break;
+      }
+      
+      if (j === needle.length - 1) {
+        return i;
+      }
+    }
+  }
+  
+  return -1;
+};
