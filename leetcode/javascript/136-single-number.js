01eac8957578fb2a9ff3c62abd220eff63ad215c
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-08-22 21:52:10 +0400

    add leetcode solution

---
+++ leetcode/javascript/136-single-number.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/single-number/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+  let result = 0;
+  for (const num of nums) {
+    result ^= num;
+  }
+  return result;
+};
