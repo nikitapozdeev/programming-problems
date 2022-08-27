d58f1c4a47aab5fb0f4181eb30fb2e0437b597e6
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-08-28 00:25:27 +0400

    add leetcode solution

---
+++ leetcode/javascript/1672-richest-customer-wealth.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/richest-customer-wealth/
+ */
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+  let max = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      sum += accounts[i][j];
+    }
+    max = Math.max(sum, max);
+  }
+  return max;
+};
