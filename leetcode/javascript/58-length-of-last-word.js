c19c6f7e0e06be5ba292213fb6299fef4b669d4d
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-11-14 00:37:04 +0400

    update leetcode problem with a better solution

--- leetcode/javascript/58-length-of-last-word.js
+++ leetcode/javascript/58-length-of-last-word.js
@@ -7,23 +7,17 @@
  * @return {number}
  */
  var lengthOfLastWord = function(s) {
-  let isWord = false;
-  let startIndex = 0;
-  let endIndex = 0;
-  for (let i = s.length; i >= 0; i--) {
-    if (!isWord) {
-      if (s[i - 1] !== ' ') {
-        isWord = true;
-        startIndex = i;
-      }
-      continue;
-    }
-    
-    if (s[i - 1] === ' ') {
-      endIndex = i;
-      break;
-    }
-  }  
+  let length = 0;
+  let i = s.length - 1;
   
-  return startIndex - endIndex;
+  while (s[i] === ' ') {
+    i--;
+  }
+  
+  while (i >= 0 && s[i] !== ' ') {
+    length++;
+    i--;
+  }
+  
+  return length;
 };
