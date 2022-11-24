7de879f4bdeab2000a0796578d92486d86f033bd
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-11-24 22:01:10 +0400

    add leetcode solution in js

---
+++ leetcode/javascript/746-min-cost-climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  for (let i = cost.length - 3; i >= 0; i--) {
+    cost[i] += Math.min(cost[i + 1], cost[i + 2]); 
+  }
+  return Math.min(cost[0], cost[1]);
+};
