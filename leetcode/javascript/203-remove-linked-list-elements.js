53c2cbf6d435642f25819d169ed52ad6a70064c4
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-12-25 16:22:58 +0400

    add leetcode solution in js

---
+++ leetcode/javascript/203-remove-linked-list-elements.js
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/remove-linked-list-elements
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  const dummy = new ListNode();
+  dummy.next = head;
+
+  let curr = head;
+  let prev = dummy;
+  while (curr) {
+    if (curr.val === val) {
+      prev.next = curr.next;
+    } else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return dummy.next;
+};
