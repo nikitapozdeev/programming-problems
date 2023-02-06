2053ed74b1b5d4d9482b4c780e110feda2adc919
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-02-06 23:16:43 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/1470-shuffle-the-array.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/shuffle-the-array
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+  const result = [];
+  for (let i = 0; i < n ; i++) {
+    result.push(nums[i]);
+    result.push(nums[i + n]);
+  }
+
+  return result;
+};
