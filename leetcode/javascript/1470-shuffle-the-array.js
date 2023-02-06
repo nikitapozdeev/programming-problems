14ac7be0ab4e6098ae2b697f2158419acf8d6d03
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-02-06 23:35:32 +0400

    add second solution for leetcode daily in js

--- leetcode/javascript/1470-shuffle-the-array.js
+++ leetcode/javascript/1470-shuffle-the-array.js
@@ -3,6 +3,7 @@
  */
 
 /**
+ * O(n) space complexity
  * @param {number[]} nums
  * @param {number} n
  * @return {number[]}
@@ -15,4 +16,28 @@ var shuffle = function(nums, n) {
   }
 
   return result;
+};
+
+/**
+ * O(1) space complexity with bit manipulation
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+  for (let i = 0; i < n ; i++) {
+    nums[i] = nums[i] << 10;
+    nums[i] |= nums[i + n];
+  }
+
+  let right = 2 * n - 1;
+  for (let left = n - 1; left >= 0; left--) {
+    const y = nums[left] & (0xfff >> 2);
+    const x = nums[left] >> 10;
+    nums[right] = y;
+    nums[right - 1] = x;
+    right -= 2;
+  }
+
+  return nums;
 };
