01cc6380e13e5ae5cc988b927192001b86a921fc
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-02-20 22:43:14 +0400

    update old leetcode solution

--- leetcode/c/35-search-insert-position.c
+++ leetcode/c/35-search-insert-position.c
@@ -2,11 +2,26 @@
  * https://leetcode.com/problems/search-insert-position/
  */
 
-int searchInsert(int* nums, int numsSize, int target){
-  for (int i = 0; i < numsSize; i++) {
-    if (nums[i] >= target) {
-      return i;
+int searchInsert(int* nums, int numsSize, int target) 
+{
+  int low = 0;
+  int high = numsSize - 1;
+  int mid = 0;
+
+  while (low <= high) {
+    mid = (low + high) >> 1;
+    int suggest = nums[mid];
+
+    if (suggest == target) {
+      return mid;
+    }
+
+    if (suggest < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-  return numsSize;
+
+  return target < nums[mid] ? mid : mid + 1;
 }
--- leetcode/javascript/35-search-insert-position.js
+++ leetcode/javascript/35-search-insert-position.js
@@ -7,11 +7,25 @@
  * @param {number} target
  * @return {number}
  */
- var searchInsert = function(nums, target) { 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= target) {
-      return i;
+var searchInsert = function(nums, target) { 
+  let low = 0;
+  let high = nums.length - 1;
+  let mid;
+
+  while (low <= high) {
+    mid = Math.floor((high + low) / 2);
+    const suggest = nums[mid];
+
+    if (suggest === target) {
+      return mid;
+    }
+    
+    if (suggest < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
-  return nums.length;
+
+  return target < nums[mid] ? mid : mid + 1;
 };
