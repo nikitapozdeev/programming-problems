bbfbc9e0e1cec55bebaaed424d0da085a3215b24
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-03-20 21:14:43 +0400

    add daily leetcode solution in js

---
+++ leetcode/javascript/605-can-place-flowers.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/can-place-flowers
+ */
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n > 0; i++) {
+    if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+      flowerbed[i] = 1;
+      n--;
+    }
+  }
+  return n === 0;
+};
