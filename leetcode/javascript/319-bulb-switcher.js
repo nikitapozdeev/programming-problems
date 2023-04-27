2f4c8498d0314e50ca36849e94f240e9623409ce
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-04-27 22:19:32 +0400

    add daily leetcode solution in js

---
+++ leetcode/javascript/319-bulb-switcher.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/bulb-switcher
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+  return Math.floor(Math.sqrt(n));
+};
