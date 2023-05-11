40c905db88fde3c261290c693c1b9aac5eabce41
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-05-11 20:40:03 +0400

    add daily leetcode solution in js

---
+++ leetcode/javascript/2629-function-composition.js
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/function-composition
+ */
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+  return (x) => {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+    return x;
+  }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
