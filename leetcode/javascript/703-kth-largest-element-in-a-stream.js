af3275e794c90c7aee1fe5e6921f11d73445b342
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-05-23 17:58:46 +0400

    add daily leetcode solution in js

---
+++ leetcode/javascript/703-kth-largest-element-in-a-stream.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-a-stream
+ */
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+  this.k = k;
+  this.heap = new PriorityQueue({ compare: (a, b) => a - b });
+  for (const num of nums) {
+    this.heap.enqueue(num);
+  }
+
+  while (this.heap.size() > k) {
+    this.heap.dequeue();
+  }
+  console.log(this.heap)
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+  this.heap.enqueue(val);
+  if (this.heap.size() > this.k) {
+    this.heap.dequeue();
+  }
+  return this.heap.front();
+};
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
