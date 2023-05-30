6a62805ff17f92beb699b06e238c93de6679fe15
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-05-30 22:14:11 +0400

    add daily leetcode solution in js

---
+++ leetcode/javascript/2693-call-function-with-custom-context.js
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/call-function-with-custom-context
+ */
+
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+  return this.apply(context, args);
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
