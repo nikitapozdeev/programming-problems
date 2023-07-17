269a82bfd5ec07e81778854ea0e8a8b895bdf0c4
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-07-17 18:12:04 +0400

    add follow up solution for leetcode problem

--- leetcode/javascript/9-is-palindrome.js
+++ leetcode/javascript/9-is-palindrome.js
@@ -3,6 +3,8 @@
  */
 
 /**
+ * Time: O(n)
+ * Memory: O(n)
  * @param {number} x
  * @return {boolean}
  */
@@ -17,5 +19,37 @@ var isPalindrome = function(x) {
     a++;
     b--;
   }
+  return true;
+};
+
+/**
+ * Follow up solution
+ * Time: O(n)
+ * Memory: O(1)
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) {
+    return false;
+  }
+
+  let divider = 1;
+  while (x >= divider * 10) {
+    divider *= 10;
+  }
+
+  while (x) {
+    const a = x % 10;
+    const b = Math.floor(x / divider);
+    
+    if (a !== b) {
+      return false;
+    }
+
+    x = Math.floor((x % divider) / 10);
+    divider /= 100;
+  }
+ 
   return true;
 };
