4e8b2e5397ae601193d2b3ab918008898c2e84a7
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2023-07-18 00:46:59 +0400

    add better solution for leetcode problem

--- leetcode/javascript/1295-find-numbers-with-even-number-of-digits.js
+++ leetcode/javascript/1295-find-numbers-with-even-number-of-digits.js
@@ -9,3 +9,26 @@
 var findNumbers = function (nums) {
   return nums.filter((num) => `${num}`.length % 2 === 0).length;
 };
+
+/**
+ * Solution without string manipulation.
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+  let evens = 0;
+
+  for (let num of nums) {
+    let digits = 0;
+    while (num > 0) {
+      digits++;
+      num = Math.floor(num / 10);
+    }
+
+    if (digits % 2 === 0) {
+      evens++;
+    }
+  }
+
+  return evens;
+};
