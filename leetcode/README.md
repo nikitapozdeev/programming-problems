72dd0dfee6050acbbddd10f3444d8e78785aa3a5
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-01-08 22:13:35 +0400

    add leetcode daily solution in js

--- leetcode/README.md
+++ leetcode/README.md
@@ -2,7 +2,7 @@
 
 |    Programming Language  |    Problems Count  | 
 |----------|:-------------:|
-| [JavaScript](https://github.com/nikitapozdeev/programming-problems/tree/master/leetcode/javascript) | 390 | 
+| [JavaScript](https://github.com/nikitapozdeev/programming-problems/tree/master/leetcode/javascript) | 391 | 
 | [Go](https://github.com/nikitapozdeev/programming-problems/tree/master/leetcode/go) | 1 | 
 | [C](https://github.com/nikitapozdeev/programming-problems/tree/master/leetcode/c) | 4 | 
 
---
+++ leetcode/javascript/938-range-sum-of-bst.js
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.com/problems/range-sum-of-bst
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+  let sum = 0;
+
+  const dfs = (node) => {
+    if (!node) {
+      return;
+    }
+
+    if (node.val >= low && node.val <= high) {
+      sum += node.val;
+    }
+
+    if (node.val > low) {
+      dfs(node.left);
+    }
+
+    if (node.val < high) {
+      dfs(node.right);
+    }
+  }
+
+  dfs(root);
+  return sum;
+};
