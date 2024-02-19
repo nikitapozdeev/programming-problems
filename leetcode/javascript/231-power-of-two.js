b1c812c0a389011b81f230c796bee6d022877971
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-02-19 21:23:38 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/231-power-of-two.js
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/power-of-two/
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+};
