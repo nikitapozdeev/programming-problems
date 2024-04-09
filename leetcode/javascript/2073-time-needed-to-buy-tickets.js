0afda85b12e4486fb8b22077e11d91d24db31366
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-04-09 23:19:09 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/2073-time-needed-to-buy-tickets.js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/time-needed-to-buy-tickets
+ */
+
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+  let time = 0;
+  
+  for (let i = 0; i < tickets.length; i++) {
+    if (i <= k) {
+      time += Math.min(tickets[i], tickets[k])
+    } else {
+      time += Math.min(tickets[i], tickets[k] - 1);
+    }
+  }
+
+  return time;
+};
