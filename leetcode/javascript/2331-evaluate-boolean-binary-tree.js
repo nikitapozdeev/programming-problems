819180411e221ce55b81131b15f2dc7e9ea44d9b
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-05-16 20:10:40 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/2331-evaluate-boolean-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/evaluate-boolean-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+  if (!root.left && !root.right) {
+    return root.val;
+  }
+
+  const left = evaluateTree(root.left);
+  const right = evaluateTree(root.right);
+  
+  return root.val === 2 
+    ? (left || right) 
+    : (left && right);
+};
