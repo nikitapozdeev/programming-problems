fa3e0e07909a6827fcbe5c2b05c2b270e68f98ca
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-06-01 19:45:34 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/3110-score-of-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/score-of-a-string
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+  let score = 0;
+  for (let i = 1; i < s.length; ++i) {
+    score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+  }
+  return score;
+};
