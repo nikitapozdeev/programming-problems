a5dc3a64286e0bf24dfea53a8e281f00844b60c5
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-06-04 09:03:19 +0400

    update leetcode solution

--- leetcode/javascript/409-longest-palindrome.js
+++ leetcode/javascript/409-longest-palindrome.js
@@ -7,21 +7,21 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-  const map = {};
-  for (let char of s) {
-    if (!map[char]) {
-      map[char] = 0;
+  let length = 0;
+  const seen = new Map();
+
+  for (const char of s) {
+    if (seen.has(char)) {
+      seen.delete(char);
+      length += 2;
+    } else {
+      seen.set(char);
     }
-    map[char]++;
   }
-  
-  let result = 0;
-  Object.values(map).forEach(cnt => {
-    result += Math.floor(cnt / 2) * 2;
-    if (result % 2 === 0 && cnt % 2 === 1) {
-      result++;
-    }
-  });
-  
-  return result;
+
+  if (seen.size) {
+    length++;
+  }
+
+  return length;
 };
