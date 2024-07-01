5080188142ba274123588271acedda5d2de01026
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-07-01 22:44:47 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/1550-three-consecutive-odds.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/three-consecutive-odds
+ */
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+  for (let i = 0; i < arr.length - 2; ++i) {
+    if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0) {
+      return true;
+    }
+  }
+  return false;
+};
