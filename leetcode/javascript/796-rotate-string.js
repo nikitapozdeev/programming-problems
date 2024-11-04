003823b5fc4a56adc7ac994ba8785bce4349a03e
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2024-11-04 14:59:21 +0400

    add leetcode daily solution in js

---
+++ leetcode/javascript/796-rotate-string.js
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/rotate-string
+ */
+
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+  if (s.length !== goal.length) return false;
+  return s.concat(s).includes(goal);
+};
